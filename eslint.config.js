9158d05b176d83995eff94e5530beacb22f33a1d
nari <yasunarj@gmail.com>
2024-07-13 16:18:50 +0900

    mod

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    // React関連のルールを無効にする
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unknown-property': 'off',
+      // その他必要なルールを追加・修正する
+    }
+  }
+];
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+const TextInput = () => {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} aria-Label="Text Input" />
+      <p>Entered Text: {text}</p>
+    </div>
+  );
+};
+
+export default TextInput;
