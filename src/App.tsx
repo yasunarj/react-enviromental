736bd75792496f53b7f95f519a33fc245c4e0b1b
nari <yasunarj@gmail.com>
2024-07-13 16:58:00 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>こんに</p>
+        <p>こんに値は</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
